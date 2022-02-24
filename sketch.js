cc4eda76a5852a130cfdf55f816b845658304907
yasminhackbart <90115526+yasminhackbart@users.noreply.github.com>
2022-02-24 18:07:21 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,40 +3,33 @@ var position;
 
 
 function setup(){
-  database = firebase.database();
-  console.log(database);
+
   createCanvas(500,500);
 
   hypnoticBall = createSprite(250,250,10,10);
   hypnoticBall.shapeColor = "red";
 
-
-  var hypnoticBallPosition = database.ref('ball/position');
-  hypnoticBallPosition.on("value", readPosition, showError);
 }
 
 function draw(){
   background("white");
   
     if(keyDown(LEFT_ARROW)){
-      writePosition(-1,0);
+      readPosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-      writePosition(1,0);
+      readPosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-      writePosition(0,-1);
+      readPosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-      writePosition(0,+1);
+      readPosition(0,+1);
     }
     drawSprites();
   
 }
 
-function writePosition(x,y){
-  
-}
 
 function readPosition(data){
   position = data.val();
@@ -45,6 +38,3 @@ function readPosition(data){
   hypnoticBall.y = position.y;
 }
 
-function showError(){
-  console.log("Error in writing to the database");
-}
